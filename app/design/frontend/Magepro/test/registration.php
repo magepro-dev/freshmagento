fc87069f3a6dcf4cc83c2f4e13eff1f702ee24ea
magepro-dev <a.pashchenko@atwix.com>
2024-04-17 18:48:19 +0300

    initial commit

---
+++ app/design/frontend/Magepro/test/registration.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Atwix Team
+ * @copyright Copyright (c) Atwix (https://www.atwix.com/)
+ */
+
+/**
+ * Copyright &copy; Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME,
+    'frontend/Magepro/test',
+    __DIR__
+);
